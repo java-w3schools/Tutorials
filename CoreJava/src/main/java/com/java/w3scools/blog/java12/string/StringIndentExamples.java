ee9b18e2a2f2399b5dd1ea3c39e9ea0b6f839358
Nukala <vnukala@paypal.com>
2019-05-08 18:47:34 +0530

    Moving to Core Java

--- CoreJava/src/main/java/com/java/w3scools/blog/java12/string/StringIndentExamples.java
+++ CoreJava/src/main/java/com/java/w3scools/blog/java12/string/StringIndentExamples.java
@@ -3,13 +3,14 @@ package com.java.w3scools.blog.java12.string;
 public class StringIndentExamples {
 	public static void main(String[] args) {
 
-		indentString();
-	}
+		String value = "indent";
 
-	public static void indentString() {
-		String value = "java-\nw3schools";
+		System.out.println(value);
+		System.out.println(value.indent(5));
+
+		value = "java-\nw3schools";
 		System.out.println(value);
 		System.out.println(value.indent(5));
 	}
-}
 
+}
