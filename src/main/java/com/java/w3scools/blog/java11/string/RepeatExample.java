36eec579674f7c9d0659b5f2b783dc5811e233ad
Nukala <vnukala@paypal.com>
2019-05-08 17:55:02 +0530

    Java 12 Repeat example

---
+++ src/main/java/com/java/w3scools/blog/java11/string/RepeatExample.java
@@ -0,0 +1,10 @@
+package com.java.w3scools.blog.java11.string;
+
+public class RepeatExample {
+
+	public static void main(String[] args) {
+		String outputString = "Hola " + "la ".repeat(4);
+		System.out.println(outputString);
+	}
+
+}
