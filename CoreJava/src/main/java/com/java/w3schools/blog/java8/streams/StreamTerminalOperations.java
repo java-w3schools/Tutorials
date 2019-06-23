3052b48d6547d7e7031cb1927b3c968a0ff701aa
Venkatesh N <venkateshn@Venkateshs-MacBook-Pro-2.local>
2019-06-23 20:00:36 +0530

    Stream api terminal operations of java 8

---
+++ CoreJava/src/main/java/com/java/w3schools/blog/java8/streams/StreamTerminalOperations.java
@@ -0,0 +1,14 @@
+package com.java.w3schools.blog.java8.streams;
+
+/**
+ * 
+ * @author java-w3schools
+ *
+ */
+public class StreamTerminalOperations {
+
+	public static void main(String[] args) {
+
+	}
+
+}
